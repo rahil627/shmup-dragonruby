bd974d3861e4d9272a67cd138be96efd4a377e16
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-10-15 05:15:00 -0700

    +notes from docs

---
+++ app/notes-ruby.rb
@@ -0,0 +1,14 @@
+# TODO: merge in ruby.txt
+
+
+# from getting started tutorial
+Array is one of the most powerful classes in Ruby and a very fundamental component of Game Toolkit.
+
+  
+args.outputs.labels << [580, 400, 'Hello World!']
+  # The "<<" thing says "append this array onto the list of them at args.outputs.labels)
+  
+args.state.rotation ||= 0
+  # shorthand for "if args.state.rotation isn't initialized, set it to zero." It's a nice way to embed your initialization code right next to where you need the variable.
+  #  - amazing..!
+
