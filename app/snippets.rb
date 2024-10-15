f95c40fc85c461f77b134fc98ae46841058d634d
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-10-15 05:40:59 -0700

    Create snippets.rb

---
+++ app/snippets.rb
@@ -0,0 +1,12 @@
+
+the samples should be more than enough..!
+
+from discord:
+...
+
+amir's gist:
+https://gist.github.com/amirrajan
+
+https://github.com/kfischer-okarin/dragonruby-toolbox
+https://github.com/kfischer-okarin/dragon_skeleton
+https://github.com/kfischer-okarin/roguelike-base/
