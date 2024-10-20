b88c05093fb933b8b4ba37e4a310bf34f393fef1
rathewolf <1132053+rahil627@users.noreply.github.com>
2024-10-19 21:25:33 -0700

    re-org, +hashsplat

--- app/notes-ruby.rb
+++ app/notes-ruby.rb
@@ -1,5 +1,11 @@
 # TODO: merge in ruby.txt
 
+# basics
+return if args.state.player[:cooldown] > 0
+  # statement if condition
+
+
+
 # functions
 def function args1 not2
   # need to add parenthesis for two or more arguments
@@ -27,8 +33,74 @@ puts add_one_to(3) # "conventional"
 puts(add_one_to 3) # outer
 puts(add_one_to(3)) # all
   
+
+
+
+# hashes and arrays
+Array is one of the most powerful classes in Ruby and a very fundamental component of Game Toolkit.
+
+A Hash has certain similarities to an Array, but:
+An Array index is always an Integer.
+A Hash key can be (almost) any object.
+
+An Array is an ordered, integer-indexed collection of objects, called elements. Any object (even another array) may be an array element, and an array can contain objects of different types.
+
+
+**hash
+*array
+  # These are splat operators, they'll deconstruct your hash/array into arguments for a method. 
+
+target = { x: 50, y: 50 }
+def move_to(x:, y:)
+  # moves player to x, y
+end
+move_to(**target)
+  # hash splat
+
+target = [50, 50]
+def move_to(x, y)
+  # moves player to x, y
+end
+move_to(*target)
+  # array splat
+
+
+old_hash = { a: 1, b: 2 }
+hash = { a: 5, **old_hash, b: 6 } # => { a: 1, b: 6 }
+  # args replaced by order of precedence
+
+old_array = [2, 3, 4 , 5]
+array = [ 1, 2, *old_array, 5, 6] # => [1, 2, 2, 3, 4, 5, 5, 6]
+  # not for arrays tho..
+
+"hashsplat is useful for moments when ~'a lot of the things i want to output are similar'"
+
+## hashes
 screen ||= {0, 0, 720, 1280} # error
 screen ||= {x: 0, y: 0, h: 720, w: 1280} # hashes need keys
+
+
+## arrays
+
+
+
+
+
+  
+
+# from getting started tutorial
+   
+args.outputs.labels << [580, 400, 'Hello World!']
+  # The "<<" thing says "append this array onto the list of them at args.outputs.labels)
+  
+args.state.rotation ||= 0
+  # shorthand for "if args.state.rotation isn't initialized, set it to zero." It's a nice way to embed your initialization code right next to where you need the variable.
+  #  - amazing..!
+
+args.state.laser.trash ||= true
+  # nested field mades on-the-fly..?
+
+
 
 # loops
 # loops are quite different..
@@ -55,29 +127,3 @@ end
 (0..3).each do |i|
   puts i
 end
-
-
-
-
-return if args.state.player[:cooldown] > 0
-  # statement if condition
-  
-# from getting started tutorial
-Array is one of the most powerful classes in Ruby and a very fundamental component of Game Toolkit.
-
-A Hash has certain similarities to an Array, but:
-An Array index is always an Integer.
-A Hash key can be (almost) any object.
-
-An Array is an ordered, integer-indexed collection of objects, called elements. Any object (even another array) may be an array element, and an array can contain objects of different types.
-   
-args.outputs.labels << [580, 400, 'Hello World!']
-  # The "<<" thing says "append this array onto the list of them at args.outputs.labels)
-  
-args.state.rotation ||= 0
-  # shorthand for "if args.state.rotation isn't initialized, set it to zero." It's a nice way to embed your initialization code right next to where you need the variable.
-  #  - amazing..!
-
-args.state.laser.trash ||= true
-  # nested field mades on-the-fly..?
-
